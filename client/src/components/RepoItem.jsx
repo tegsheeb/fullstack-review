c139919cd04e90beba4d3338168f977a06069377
tegshee <tegshbayar@gmail.com>
2020-06-13 11:19:04 -0700

    Display top 25 withouth refresh after new handle added

--- client/src/components/RepoItem.jsx
+++ client/src/components/RepoItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RepoItem = (props) => {
   return (
     <div>
-      <div>Repo Name:  {props.repo.repo_name}, Owner: {props.repo.owner_username}, Forks: {props.repo.fork_counts}</div>
+      <div>Repo Name:  {props.repo.repo_name}, Owner: {props.repo.owner_username}, Forks: {props.repo.fork_counts} , Visit the repo <a href={props.repo.html_url}> here </a> </div>
     </div>
   )
 }
