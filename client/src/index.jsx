c881036ab1d17173b2135fdf60695a055f214972
tegshee <tegshbayar@gmail.com>
2020-06-13 12:53:17 -0700

    Refactor console.logs

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -38,7 +38,7 @@ class App extends React.Component {
       .then(res => res.json())
       .then(
         (result) => {
-          console.log(result);
+          // console.log(result);
           this.setState({
             isLoaded: true,
             repos: result,
